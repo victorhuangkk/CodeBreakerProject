baabd57d95570b30ec0a1f696e4a939e2bda79f9
victorhuangkk <32436601+victorhuangkk@users.noreply.github.com>
2018-03-31 14:26:58 -0700

    edit1
    first attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,22 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+    if (answer == '' | attempt == ''){
+       setHiddenFields();
+    }
     //add functionality to guess function here
 }
 
 //implement new functions here
+
+function setHiddenFields () {
+	let temp = Math.random();
+	let variable = 10000*Math.floor(temp);
+    let answer = variable.toString();
+    var i = answer.length;
+    while(i < 4) {
+    	i++;
+        answer = "0" + answer;
+    }
+    document.setHiddenFields("attempt").value = 0;
+}
